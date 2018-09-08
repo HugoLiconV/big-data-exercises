f91a0df80112a9d333c8e73334db2048bc986a68
Hugo <hugo_val28@hotmail.com>
2018-09-08 15:20:14 -0600

    setup project

---
+++ src/main/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -0,0 +1,31 @@
+package nearsoft.academy.bigdata.recommendation;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Hugo on 06/09/18.
+ */
+public class MovieRecommender {
+
+
+  public MovieRecommender(String path) {}
+
+  public int getTotalReviews() {
+    return 0;
+  }
+
+  public int getTotalProducts() {
+    return 0;
+  }
+
+  public int getTotalUsers() {
+    return 0;
+  }
+
+  public List<String> getRecommendationsForUser(String userId) {
+
+    return new ArrayList<>();
+  }
+}
